3f1e126c1f3f39d137c17b711b51a7a01643d027
Ritikweb <ritikrs918@gmail.com>
2022-01-24 19:30:18 +0530

    :)

---
+++ Basic of Programming in JavaScript/template-string.js
@@ -0,0 +1,9 @@
+// template string -- to pick up the value from variables
+
+let age = 20;
+let firstName = "Ritik";
+
+let aboutMe = `my name is ${firstName} and I am ${age} years old`;
+console.log(aboutMe) // my name is Ritik and I am 20 years old
+
+// use backticks (``) for template strings

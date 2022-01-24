441c6aea813e549f832e0d3b68f1429826cdd916
Ritikweb <ritikrs918@gmail.com>
2022-01-24 19:17:07 +0530

    complete

---
+++ Basic of Programming in JavaScript/string-concatenation.js
@@ -0,0 +1,10 @@
+// string concatenation -- combining strings
+
+let str1 = "Ritik";
+let str2 = "Singh";
+
+let concat = str1 + str2;
+console.log(concat); // RitikSingh
+
+let concat2 = str1 + " " + str2;
+console.log(concat2); // Ritik Singh

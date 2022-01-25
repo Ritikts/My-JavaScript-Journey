df8d9a458ced42c1dd572931ce0c1b74ccf08dae
Ritikweb <ritikrs918@gmail.com>
2022-01-25 11:21:01 +0530

    complete

---
+++ Basic of Programming in JavaScript/booleans & comparison operator.js
@@ -0,0 +1,20 @@
+// booleans and comparison operator
+
+let num1 = 4;
+let num2 = 2;
+console.log(num1 < num2); // false
+console.log(num1 > num2); // true
+
+// == vs ===
+
+let num3 = 8;
+let num4 = "8";
+console.log(num3 == num4); // true because == only check the value not type
+console.log(num3 === num4); // false because === check value and type 
+
+// != vs !== 
+
+let num5 = 6;
+let num6 = "6";
+console.log(num5 != num6); // false because != only check the value not type
+console.log(num5 !== num6); // true because !== check value and type

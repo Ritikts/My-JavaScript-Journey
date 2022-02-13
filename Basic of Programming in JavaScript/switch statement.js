cad94dd2aad4317c1f35979226f6c495eb6a6b71
Ritikweb <ritikrs918@gmail.com>
2022-02-13 14:08:43 +0530

    switch statement complete

--- Basic of Programming in JavaScript/switch statement.js
+++ Basic of Programming in JavaScript/switch statement.js
@@ -1,11 +1,31 @@
 // switch statement
 
-let day = prompt("Enter the day from");
+let day = Number(prompt("Enter the number form 0 to 6")); // prompt always accept the values as a string so we use number method to accept the values as a number
 switch (day) {
-    case(1) : 
-           
-        break;
-
-    default:
-        break;
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Please enter the number between 0 and 6");
+    break;
 }
+
+// if we don't use break here then it will print all the console log

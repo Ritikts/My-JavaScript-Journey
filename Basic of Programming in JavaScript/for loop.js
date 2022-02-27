aca25163276397f00218b7903e8146523ca85374
Ritikweb <ritikrs918@gmail.com>
2022-02-27 18:32:38 +0530

    for loop

---
+++ Basic of Programming in JavaScript/for loop.js
@@ -0,0 +1,14 @@
+// print 0 to 9
+
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
+// 0 1 2 3 4 5 6 7 8 9 10
+
+// print sum of first 100 natural numbers
+let total = 0;
+let num = 100;
+for (i = 0; i <= num; i++) {
+  total = total + i;
+}
+console.log(total);
